4541710cd3a87e4171aebdc14f18c47f3e9456c1
XTYGitTemp <2765252307@qq.com>
2020-11-04 13:04:20 +0800

    第十五次课

---
+++ 第十四次课/demo14.js
@@ -0,0 +1,9 @@
+function apple() {
+    var array = [12,67,89,100,84,38,64,52];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index]; 
+        console.log(element);     
+    }
+    
+}
+apple();
