c4ac3ad7f0329ef5749619c4d46db69d99a47bf3
XTYGitTemp <2765252307@qq.com>
2020-12-09 10:14:22 +0800

    第十八次课

---
+++ 第十八次课/原型的自定义.js
@@ -0,0 +1,16 @@
+
+Object2.prototype2 = {
+    __proto2__ = null
+}
+
+function Object2(){
+    this.__proto2__ = Object2.prototype2;
+
+}
+
+
+function Haw3 (){
+    console.log("this is haw");
+}
+
+Haw.name = ""
